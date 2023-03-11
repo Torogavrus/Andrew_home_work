3c3b0b5035ce8c75c79d5a605aaf264b4291b428
AndriyTurko <andriyturko33@gmail.com>
2023-03-11 13:53:46 +0000

    my home work 11.03

---
+++ test_02_28.py
@@ -0,0 +1,76 @@
+# my home-work
+
+a = "hello world"
+a += " !"
+print(a)
+
+a += "jfkdskba " \
+    + "thsdsathuady"
+
+print(a)
+
+q = ['a', 'b', 'c', 'd', 'e']
+x = 0
+while x < 5:
+    print(q[x])
+    x += 1
+
+print('---------------------------------------------------------------')
+
+w = 0
+while w < len(a):
+    if a[w] in ['l', 's']:
+        w += 1
+        continue
+    print(a[w])
+    w += 1
+    if a[w] == 'u':
+        break
+
+print('----------------------------------')
+
+
+s = {'x': 10, 'q': 76, 'a': 9, 'b': 20, 'c': 3}
+
+s_keys = list(s.keys())
+print(s_keys)
+s_values = list(s.values())
+print(s_values)
+
+o = 0
+
+print('-----------------------')
+while o < len(s):
+    if s_keys[o] == 'a':
+        break
+    print(s[s_keys[o]])
+    print(s_keys[o])
+    o += 1
+
+    #print(list(s.keys()[o]))
+    #print(list(s.values())[o]
+print('------------------------')
+
+while o < len(s):
+    if s_values[o] == 20:
+        continue
+    print(s[s_keys[o]])
+    print(s_keys[o])
+    o += 1
+
+
+print('------------------------')
+
+for x in s_keys:
+    if x == 'a':
+        break
+    print(s[x])
+    print(x)
+
+
+
+
+
+
+
+
